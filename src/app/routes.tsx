fcb4feb5b64ca69b52bd6b922d0a0ee2a589fa1d
parkjihyoun <jihyoun2002@knu.ac.kr>
2026-01-24 23:29:57 -0600

    feat: 인증 라우팅 구성

---
+++ src/app/routes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom"
+import LoginPage from "../pages/LoginPage"
+import AuthCallbackPage from "../pages/AuthCallbackPage"
+import ConsentPage from "../pages/ConsentPage"
+import ErrorPage from "../pages/ErrorPage"
+
+export const router = createBrowserRouter([
+    { path: "/", element: <LoginPage /> },
+    { path: "/auth/callback", element: <AuthCallbackPage /> },
+    { path: "/consent", element: <ConsentPage /> },
+    { path: "/error", element: <ErrorPage /> },
+])
